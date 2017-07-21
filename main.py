6bd2fa27b0eb86d584f136347c27482f6cfd88a6
Fascha <Fabian.Schatz@googlemail.com>
2017-07-21 15:45:15 +0200

    reworked the Mapping class; can now be set up with a destination width to match a given monitor/projection; to calculate a new matrix the method calculate_source_to_dest needs to be called with exactly 4 IR marker positions as Tuples; to get the mapped center of the wiimote ircam the method get_pointing_point has to be called;

--- main.py
+++ main.py
@@ -42,6 +42,83 @@ TODO:
 
 class Mapping:
 
+    def __init__(self, dest_w, dest_h):
+        self.SRC_W = 1024
+        self.SRC_H = 768
+        self.DEST_W = dest_w
+        self.DEST_H = dest_h
+
+        self.source_to_dest = None
+        self.dx1, self.dy1 = 0, 0
+        self.dx2, self.dy2 = self.DEST_W, 0
+        self.dx3, self.dy3 = self.DEST_W, self.DEST_H
+        self.dx4, self.dy4 = 0, self.DEST_H
+
+    def calculate_source_to_dest(self, ir_markers):
+        if len(ir_markers) == 4:
+            x1, y1 = ir_markers[0]
+            x2, y2 = ir_markers[1]
+            x3, y3 = ir_markers[2]
+            x4, y4 = ir_markers[3]
+
+            # Step 1
+            source_points_123 = np.matrix([[x1, x2, x3],
+                                           [y1, y2, y3],
+                                           [1, 1, 1]])
+
+            source_point_4 = [[x4],
+                              [y4],
+                              [1]]
+
+            scale_to_source = np.linalg.solve(source_points_123, source_point_4)
+
+            ls, ms, ts = [float(x) for x in scale_to_source]
+
+            # Step 2
+            unit_to_source = np.matrix([[ls*x1, ms*x2, ts*x3],
+                                        [ls*y1, ms*y2, ts*y3],
+                                        [ls*1, ms*1, ts*1]])
+
+            # Step 3
+            dest_points_123 = np.matrix([[self.dx1, self.dx2, self.dx3],
+                                         [self.dy1, self.dy2, self.dy3],
+                                         [1, 1, 1]])
+
+            dest_point_4 = np.matrix([[self.dx4],
+                                      [self.dy4],
+                                      [1]])
+
+            scale_to_dest = np.linalg.solve(dest_points_123, dest_point_4)
+
+            ld, md, td = [float(x) for x in scale_to_dest]
+
+            unit_to_dest = np.matrix([[ld*self.dx1, md*self.dx2, td*self.dx3],
+                                      [ld*self.dy1, md*self.dy2, td*self.dy3],
+                                      [ld*1, md*1, td*1]])
+
+            # Step 4
+            source_to_unit = np.linalg.inv(unit_to_source)
+
+            # Step 5
+            self.source_to_dest = unit_to_dest @ source_to_unit
+        else:
+            print("NOT EXACTLY 4 IR MARKER COORDINATES GIVEN")
+            return
+
+
+    def get_pointing_point(self):
+        # Mapping of center
+        x, y, z = [float(w) for w in self.source_to_dest @ np.matrix([[self.SRC_W/2],
+                                                                      [self.SRC_H/2],
+                                                                      [1]])]
+
+        # step 7: dehomogenization
+        x = x/z
+        y = y/z
+        return x, y
+
+class Mapping_safe:
+
     SRC_W = 1024
     SRC_H = 768
 
@@ -123,7 +200,6 @@ class Mapping:
                                                                        [1]]))]
 
         return self.dehomogenize(x, y, z)
-
 
     def dehomogenize(self, x, y, z):
         return x/z, y/z
@@ -697,6 +773,7 @@ class PaintApplication:
                 print()
 
                 if self.paint_area.drawing:
+                    # rechts links vertauscht
                     self.paint_area.paint_objects.append(Pixel(mapped_data[1], mapped_data[0], self.paint_area.active_color, self.paint_area.active_size))
                     # for ir_object in ir_data:
                     #     if ir_object['id'] < 50:
