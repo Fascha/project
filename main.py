a442a0344562952cf28d041aebad39a017c03dc6
mjakob93 <marco@enerix.de>
2017-07-23 17:14:10 +0200

    added pre-defined gestures and connected them to functions in "main" for operations

--- main.py
+++ main.py
@@ -508,6 +508,7 @@ class PaintApplication:
         self.gesture_recognition = GestureRecognition()
         self.recognition_data = []
         self.recognition_mode_enabled = False
+        self.selection_mode = 'standard'
 
         self.mapping = Mapping(1920, 1080)
         print("ASSERTED: (99.44448537537721, 847.1789582258892)")
@@ -692,21 +693,35 @@ class PaintApplication:
         if len(self.recognition_data) > 0:
             gesture = self.gesture_recognition.get_gesture(self.recognition_data)
             # handle gesture etc
-            print(gesture)
-            print(gesture.name)
+            print(self.recognition_data)
+            # print(gesture)
+            # print(gesture.name)
             self.handle_gesture(gesture)
 
         # self.set_recognition_mode(False)
 
     def handle_gesture(self, gesture):
         if gesture.name == 'Swipe left':
-            self.paint_area.undo_drawing()
+            if self.selection_mode == 'standard':
+                self.paint_area.undo_drawing()
+            elif self.selecion_mode == 'colorpicker':
+                # nextcolor
+                pass
         elif gesture.name == 'Swipe right':
-            self.paint_area.redo_drawing()
+            if self.selection_mode == 'standard':
+                self.paint_area.redo_drawing()
+            elif self.selection_mode == 'colorpicker':
+                # previouscolor
+                pass
         elif gesture.name == 'Circle clockwise':
             self.shape_picker.btn_shapes['CIRCLE'].click()
         elif gesture.name == 'Circle counterclockwise':
             self.shape_picker.btn_shapes['LINE'].click()
+        elif gesture.name == 'C_shape':
+            self.selection_mode = 'colorpicker'
+        elif gesture.name == 'mirrored_C_shape':
+            self.selection_mode = 'standard'
+            print("standard")
 
     def handle_ir_data(self, ir_data):
 
